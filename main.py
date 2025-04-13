cc7a1a88ac0d4b43b942f46be6723e0939e473f3
Mykola Dziuba <mykola.dziuba92@gmail.com>
2025-04-13 11:31:57 +0000

    Stage Testing: apply Ruff formatting fixes

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-def main():
-    print("Hello from mlops!")
+import os
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+import yaml
+
+
+def export_envs(environment: str = "dev") -> None:
+    env_file = f"config/.env.{environment}"
+    if not os.path.exists(env_file):
+        raise FileNotFoundError(f"{env_file} not found")
+    load_dotenv(dotenv_path=env_file, override=True)
+
+    # Debug print
+    print("DEBUG ENVIRONMENT:", os.environ.get("ENVIRONMENT"))
+    print("DEBUG APP_NAME:", os.environ.get("APP_NAME"))
+
+
+# Load secrets.yaml and export as environment variables
+with open("secrets.yaml", "r") as f:
+    secrets = yaml.safe_load(f)
+    for key, value in secrets.items():
+        os.environ[key] = str(value)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
+    print("FAKE_API_KEY:", settings.FAKE_API_KEY)
---
+++ tests/test_settings.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + "/.."))
+
+from settings import Settings
+
+
+def test_settings_loaded():
+    settings = Settings()
+    assert settings.ENVIRONMENT == "test"
+    assert settings.APP_NAME == "mlops-test"
+    assert settings.FAKE_API_KEY == "test-key-123"
